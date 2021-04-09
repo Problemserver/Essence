8818c996cf1f35fa819741ba0771e8835e2eeb06
JustFloooo <florian.spruenken@rwth-aachen.de>
2021-04-09 04:52:59 +0200

    change to java 11

--- src/essence/commands/gm.java
+++ src/essence/commands/gm.java
@@ -6,12 +6,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+
 public class gm implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(@Nonnull CommandSender commandSender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] strings) {
 
-        if (commandSender instanceof Player player) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
 
             if (strings.length == 0) {
                 player.setGameMode(player.getGameMode() == GameMode.CREATIVE ? GameMode.SURVIVAL : GameMode.CREATIVE);
@@ -23,10 +26,18 @@ public class gm implements CommandExecutor {
                 if (gmValue < 0 || gmValue > 3) return false;
 
                 switch (gmValue) {
-                    case 0 -> player.setGameMode(GameMode.SURVIVAL);
-                    case 1 -> player.setGameMode(GameMode.CREATIVE);
-                    case 2 -> player.setGameMode(GameMode.ADVENTURE);
-                    case 3 -> player.setGameMode(GameMode.SPECTATOR);
+                    case 0:
+                        player.setGameMode(GameMode.SURVIVAL);
+                        break;
+                    case 1:
+                        player.setGameMode(GameMode.CREATIVE);
+                        break;
+                    case 2:
+                        player.setGameMode(GameMode.ADVENTURE);
+                        break;
+                    case 3:
+                        player.setGameMode(GameMode.SPECTATOR);
+                        break;
                 }
 
             } else {
