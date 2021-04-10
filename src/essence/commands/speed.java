facffb01091949ea92bee82763628c18b4a47473
Nico Nußbaum <niconuss123@gmail.com>
2021-04-10 15:40:26 +0200

    try

--- src/essence/commands/speed.java
+++ src/essence/commands/speed.java
@@ -18,22 +18,23 @@ public class speed implements CommandExecutor {
     public boolean onCommand(@Nonnull CommandSender commandSender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] strings) {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
-            int newSpeed;
+            Float newSpeed;
 
             if(strings.length == 0){
-                if(player.getFlySpeed() == 1 && player.getWalkSpeed() == 1){
-                    newSpeed = 2;
+                /*if(player.getFlySpeed() == 1 && player.getWalkSpeed() == 1){
+                    newSpeed = 2f;
                     player.setWalkSpeed(newSpeed);
                     player.setFlySpeed(newSpeed);
                 }
                 else{
-                    newSpeed = 1;
+                    newSpeed = 1f;
                     player.setWalkSpeed(newSpeed);
                     player.setFlySpeed(newSpeed);
                 }
-                player.sendMessage(String.format(Language.getStringFromKeyword(LanguageKeyword.CMD_SPEED_SET), newSpeed));
+                player.sendMessage(String.format(Language.getStringFromKeyword(LanguageKeyword.CMD_SPEED_SET), newSpeed));*/
+                player.sendMessage(player.getWalkSpeed() + "");
             }else if(strings.length == 1 && strings[0].matches("[+-]?\\d*(\\.\\d+)?")){
-                newSpeed = Integer.parseInt(strings[0]);
+                newSpeed = Float.parseFloat(strings[0]);
                 player.setWalkSpeed(newSpeed);
                 player.setFlySpeed(newSpeed);
                 player.sendMessage(String.format(Language.getStringFromKeyword(LanguageKeyword.CMD_SPEED_SET), newSpeed));
